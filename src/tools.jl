aae7ce73b8f8aa6a0f36261e032970e049ae8fdd
Bonan Zhu <zhubonan@outlook.com>
2023-01-07 18:16:09 +0000

    Code refectoring

--- src/tools.jl
+++ src/tools.jl
@@ -79,10 +79,28 @@ Build structure using `buildcell` and return the relaxed structure.
 - `init_structure_transform`: A function that transforms the initial structure. If `nothing` is returned, skip this generated structure.
 """
 function build_and_relax(seedfile::AbstractString, ensemble, cf; timeout=10, nmax=500, pressure_gpa=0.0,
+    relax_cell=true,
     show_trace=false, method=TwoPointSteepestDescent(), core_size=1.0, init_structure_transform=nothing, kwargs...)
 
-    local cell
+    cell = build_one(seedfile;timeout, init_structure_transform)
+
+    calc = EDDP.NNCalc(cell, cf, ensemble; nmax, core=CoreReplusion(core_size))
+    vc, res = relax!(calc;relax_cell, pressure_gpa, show_trace, method, kwargs...)
+    vc, res
+end
+
+"""
+    build_one(seedfile;timeout=10, init_structure_transform=nothing)
+
+Build a single structure via `buildcell`.
+"""
+function build_one(seedfile;timeout=10, init_structure_transform=nothing, max_attemp=100)
+    local cell::Cell{Float64}
+    i = 1
     while true
+        if i >= max_attemp
+            throw(ErrorException("Maximum attempt for building structure exceeded!"))
+        end
         lines = open(seedfile, "r") do seed
             cellout = read(pipeline(`timeout $(timeout) buildcell`, stdin=seed, stderr=devnull), String)
             split(cellout, "\n")
@@ -95,37 +113,37 @@ function build_and_relax(seedfile::AbstractString, ensemble, cf; timeout=10, nma
             cell = init_structure_transform(cell)
             if isnothing(cell)
                 # This generated structure is no good....
+                i += 1
                 continue
             end
         end
         break
     end
+    return cell
+end
 
-    calc = EDDP.NNCalc(cell, cf, ensemble; nmax, core=CoreReplusion(core_size))
 
-    # Setup variable cell relaxation
-    p = pressure_gpa / 160.21766208
-    ext = diagm([p, p, p])
+"""
+    relax!(calc::NNCalc;relax_cell=true, show_trace, method, opt_kwargs...)
 
-    vc = EDDP.VariableCellCalc(calc, external_pressure=ext)
+Relax the structure of the calculator.
+"""
+function relax!(calc::NNCalc;relax_cell=true, pressure_gpa, show_trace, method, opt_kwargs...)
 
-    # Run optimisation
-    res = EDDP.optimise!(vc; show_trace, method, kwargs...)
+    if relax_cell
+        p = pressure_gpa / 160.21766208
+        ext = diagm([p, p, p])
+        vc = EDDP.VariableCellCalc(calc, external_pressure=ext)
+        # Run optimisation
+        res = EDDP.optimise!(vc; show_trace, method, opt_kwargs...)
+    else
+        vc = calc
+        res = EDDP.optimise!(calc; show_trace, method, opt_kwargs...)
+    end
+    update_metadata!(vc, "eddp-output")
     vc, res
 end
 
-"""
-    random_from_buildcell(seedfile; timeout=60)
-
-Run the `buildcell` progress with a defined timeout value.
-"""
-function random_from_buildcell(seedfile; timeout=60)
-    lines = open(seedfile, "r") do seed
-        cellout = read(pipeline(`timeout $(timeout) buildcell`, stdin=seed, stderr=devnull), String)
-        split(cellout, "\n")
-    end
-    CellBase.read_cell(lines)
-end
 
 """
     build_random_structures(seedfile, outdir;n=1, show_progress=false, timeout=60)
@@ -138,7 +156,7 @@ function build_random_structures(seedfile, outdir;n=1, show_progress=false, time
         prog = Progress(n)
     end
     while i< n 
-        cell = random_from_buildcell(seedfile;timeout)
+        cell = build_one(seedfile;timeout)
         label = EDDP.get_label(EDDP.stem(seedfile))
         cell.metadata[:label] = label
         if outfmt == "res"
@@ -164,6 +182,8 @@ Perform random structure searching using the seed file.
 function run_rss(seedfile, ensemble, cf;show_progress=false, max=1, outdir="./", packed=false,
                 ensemble_std_max=-1., ensemble_std_min=-1.,
                 init_structure_transform=nothing,
+                composition_engmin=Dict{Composition, Float64}(),
+                eng_threshold=-1.,
                 niggli_reduce_output=true, max_err=10, kwargs...)
     i = 1
 
@@ -176,46 +196,43 @@ function run_rss(seedfile, ensemble, cf;show_progress=false, max=1, outdir="./",
     else
         mode = "w"
     end
-    nerr = 0
     if show_progress
         pmeter = Progress(max)
     end
     while i <= max
-        local vc
-        try
-            vc, res = build_and_relax(seedfile, ensemble, cf;init_structure_transform, kwargs...)
-        catch err
-            isa(err, InterruptException) && throw(err)
-            if typeof(err) <: ProcessFailedException
-                @warn " `buildcell` failed to make the structure"
-            else
-                @warn "relaxation errored with $err"
-            end
-            if nerr >= max_err
-                @error "Maximum $(max_err) consecutive errors reached!"
-                 throw(err)
-            end
-            nerr += 1
-            continue
-        end
+        vc, res = build_and_relax_one(seedfile, ensemble, cf;max_err, init_structure_transform, kwargs...)
 
         # Check for ensemble error and act
         # This prunes structure that we are too confident, e.g. nothing to learn from
         if ensemble_std_min > 0. || ensemble_std_max > 0.
-            estd = get_energy_std(vc.calc)
-            if estd < ensemble_std_min || estd > ensemble_std_max
+            estd = get_energy_std(vc.calc) / length(get_cell(vc))
+            if estd < ensemble_std_min
+                @info "Ensemble standard deviation $(estd) is too small ($(ensemble_std_max))!"
+                continue 
+            end
+            if  estd > ensemble_std_max
+                @info "Ensemble standard deviation $(estd) is too large ($(ensemble_std_max))!"
                 continue 
             end
         end
 
-        # Reset the error counter
-        nerr = 0
- 
+        # Check if the final energy is low enough
+        # Use this with case as pathelogicial structures may prevent normal
+        # structures bening accepted. Ideally use it with `ensemble_std_min`.
+        if eng_threshold > 0
+            if check_energy_threshold!(composition_engmin, vc, eng_threshold) == false
+                continue
+            end
+        end
+
+        # Update the label of the structure
         label = EDDP.get_label(EDDP.stem(seedfile))
         EDDP.update_metadata!(vc, label)
+
         if !packed
             outfile = joinpath(outdir, "$(label).res")
         end
+
         # Write output file
         cell = get_cell(vc)
         # Run niggli reduction - skip the loop if failed.
@@ -235,135 +252,67 @@ function run_rss(seedfile, ensemble, cf;show_progress=false, max=1, outdir="./",
 end
 
 
-
 """
-    build_and_relax_one(seedfile::AbstractString, outdir::AbstractString, ensemble, cf;timeout=10, warn=true)
+    build_and_relax_one(seedfile::AbstractString, ensemble, cf::CellFeature;)
 
 Build and relax a single structure, ensure that the process *does* generate a new structure.
 """
-function build_and_relax_one(seedfile::AbstractString, outdir::AbstractString, ensemble, cf; nmax=500, timeout=10, warn=true, max_attempts=999, write=true)
-    not_ok = true
-    n = 1
-    relaxed = nothing
-    while not_ok && n <= max_attempts
+function build_and_relax_one(seedfile::AbstractString, ensemble, cf::CellFeature; 
+    init_structure_transform=nothing,
+    max_err=10, kwargs...)
+    nerr = 1
+    local vc
+    local res
+    while true
         try
-            relaxed = build_and_relax(seedfile, outdir, ensemble, cf; timeout, write, nmax)
+            vc, res = build_and_relax(seedfile, ensemble, cf;init_structure_transform, kwargs...)
         catch err
-            if !isa(err, InterruptException)
-                if warn
-                    if typeof(err) <: ProcessFailedException
-                        @warn " `buildcell` failed to make the structure"
-                    else
-                        @warn "relaxation errored with $err"
-                    end
-                end
+            isa(err, InterruptException) && throw(err)
+            if typeof(err) <: ProcessFailedException
+                @warn " `buildcell` failed to make the structure"
             else
-                # Throw Ctrl-C interruptions
+                @warn "relaxation errored with $err"
+            end
+            if nerr >= max_err
+                @error "Maximum $(max_err) consecutive errors reached!"
                 throw(err)
             end
-            n += 1
+            nerr += 1
             continue
         end
-        not_ok = false
+
+        break
     end
-    relaxed
+    vc, res
 end
 
 """
-    worker_build_and_relax_one(channel::AbstractChannel, args...; kwargs...)
+    check_energy_threshold!(composition_engmin, calc, threshold)
 
-Worker function that put the results into the channel
+Check if the structure is too high in energy and update the per-composition lowest
+energy seen so far.
 """
-function worker_build_and_relax_one(job_channel, result_channel, seed_file, outdir, ensemble, cf;
-    nmax=500, timeout=10, warn=true, max_attempts=999, write=true)
-    @info "Starting worker function"
-    while true
-        job_id = take!(job_channel)
-        if job_id < 0
-            break
+function check_energy_threshold!(composition_engmin, calc, threshold)
+    # Is the energy low enough?
+    epa = get_energy(calc) / length(get_cell(calc))
+    reduced_comp = reduce_composition(Composition(get_cell(calc)))
+    if reduced_comp in keys(composition_engmin)
+        # Update the lowest energy seen so far
+        if epa < composition_engmin[reduced_comp]
+            composition_engmin[reduced_comp] = epa
         end
-        relaxed = build_and_relax_one(seed_file, outdir, ensemble, cf; nmax, timeout, warn, max_attempts, write)
-        put!(result_channel, relaxed)
+
+        # Reject the structure if the energy is too high
+        if eng_threshold > 0 && epa > (composition_engmin[reduced_comp] + threshold)
+            return false
+        end
+    else
+        # No records so far - update the record
+        composition_engmin[reduced_comp] = epa
     end
+    return true
 end
 
-"""
-    build_and_relax(num::Int, seedfile::AbstractString, outdir::AbstractString, ensemble, cf;timeout=10)
-
-Build and relax `num` structures in parallel (threads) using passed `ModuleEnsemble` and `CellFeature`
-"""
-function build_and_relax(num::Int, seedfile::AbstractString, outdir::AbstractString, ensemble, cf; timeout=10, nmax=500, deduplicate=false)
-    results_channel = RemoteChannel(() -> Channel(num))
-    job_channel = RemoteChannel(() -> Channel(num))
-
-    # Put the jobs
-    for i = 1:num
-        put!(job_channel, i)
-    end
-
-    @info "Launching workers"
-
-    # Launch the workers
-    futures = []
-    for worker in workers()
-        push!(futures, remotecall(worker_build_and_relax_one, worker, job_channel, results_channel, seedfile, outdir, ensemble, cf; timeout, warn=true, write=false, nmax))
-    end
-    sleep(0.1)
-    # None of the futures should be ready
-    for future in futures
-        if isready(future)
-            output = fetch(future)
-            @error "Error detected for the worker $output"
-            throw(output)
-        end
-    end
-
-    @info "Start the receiving loop"
-    # Receive the data and update the progress
-    i = 1
-    progress = Progress(num)
-    # Fingerprint vectors used for deduplication
-    all_fvecs = []
-    try
-        while i <= num
-            res = take!(results_channel)
-            label = res.metadata[:label]
-            # Get feature vector
-            # Compare feature vector
-            if deduplicate
-                fvec = CellBase.fingerprint(res)
-                if is_unique_fvec(all_fvecs, fvec)
-                    push!(all_fvecs, fvec)
-                    # Unique structure - write it out
-                    write_res(joinpath(outdir, "$(label).res"), res)
-                    ProgressMeter.next!(progress;)
-                    i += 1
-                else
-                    @warn "This structure has been seen before"
-                    # Resubmit the job
-                    put!(job_channel, i)
-                end
-            else
-                write_res(joinpath(outdir, "$(label).res"), res)
-                ProgressMeter.next!(progress;)
-                i += 1
-            end
-        end
-    catch err
-        if isa(err, InterruptException)
-            # interrupt workers
-            Distributed.interrupt()
-        else
-            throw(err)
-        end
-    finally
-        foreach(x -> put!(job_channel, -1), 1:length(workers()))
-        sleep(1.0)
-        close(job_channel)
-        close(results_channel)
-    end
-
-end
 
 """
 Check if a feature vector already present in an array of vectors
@@ -380,18 +329,6 @@ function is_unique_fvec(all_fvecs, fvec; tol=1e-2, lim=5)
     !match
 end
 
-"""
-    build_and_relax(num::Int, seedfile::AbstractString, outdir::AbstractString, ensemble_file::AbstractString;timeout=10)
-
-Build the structure and relax it
-"""
-function build_and_relax(num::Int, seedfile::AbstractString, outdir::AbstractString, ensemble_file::AbstractString; timeout=10, kwargs...)
-    ensemble = load_ensemble_model(ensemble_file)
-    featurespec = load_featurespec(ensemble_file)
-    build_and_relax(num, seedfile, outdir, ensemble, featurespec; timeout, kwargs...)
-end
-
-
 ensure_dir(path) = isdir(path) || mkdir(path)
 
 function get_label(seedname)
@@ -401,48 +338,6 @@ function get_label(seedname)
 end
 
 stem(x) = splitext(splitpath(x)[end])[1]
-
-"""
-Call `buildcell` to generate many random structure under `outdir`
-"""
-function build_cells(seedfile, outdir, num; save_as_res=true, build_timeout=5, ntasks=nthreads())
-    asyncmap((x) -> build_one_cell(seedfile, outdir; save_as_res, build_timeout), 1:num;
-        ntasks=ntasks)
-end
-
-"""
-Call `buildcell` to generate many random structure under `outdir`
-"""
-function build_one_cell(seedfile, outdir; save_as_res=true, build_timeout=5, suppress_stderr=false, max_attemps=999)
-    not_ok = true
-    suppress_stderr ? stderr_dst = devnull : stderr_dst = nothing
-    n = 1
-    while not_ok && n <= max_attemps
-        outname = save_as_res ? get_label(stem(seedfile)) * ".res" : get_label(stem(seedfile)) * ".cell"
-        outpath = joinpath(outdir, outname)
-        try
-            if save_as_res
-                pip = pipeline(pipeline(`timeout $(build_timeout) buildcell`, stdin=seedfile), `timeout $(build_timeout) cabal cell res`)
-                pip = pipeline(pip, stdout=outpath, stderr=stderr_dst)
-            else
-                pip = pipeline(`timeout $(build_timeout) buildcell`, stdin=seedfile, stdout=outpath, stderr=stderr_dst)
-            end
-
-            run(pip)
-        catch err
-            if typeof(err) <: ProcessFailedException
-                rm(outpath)
-                n += 1
-                continue
-            else
-                throw(err)
-            end
-        end
-        # Success
-        not_ok = false
-    end
-end
-
 
 swapext(fname, new) = splitext(fname)[1] * new
 
